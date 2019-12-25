5cf0d74f6da5ffe7dac7cf90905bfb13c24df710
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-25 16:56:48 +0600

    Project created

---
+++ casting_libs/src/main/java/org/richit/casting_libs/MyClass.java
@@ -0,0 +1,17 @@
+package org.richit.casting_libs;
+
+public class MyClass {
+    static String dot = "----------------";
+    static String start = "Programme started";
+    static String end = "Programme ended";
+
+    public static void main(String[] args) {
+        System.out.println(dot+start+dot);
+        System.out.println("");
+
+
+
+        System.out.println("");
+        System.out.println(dot+end+dot);
+    }
+}
